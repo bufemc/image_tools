f41e03b663792dafdec0c8b3d92a14f207593c6f
Christoph G. Keller <christoph.keller@gmx.net>
2015-06-03 11:21:12 +0200

    now catching situation when there are nor files matching the pattern

--- rawimage-datesort.py
+++ rawimage-datesort.py
@@ -1,6 +1,6 @@
 #!/opt/local/bin/python
 ##########################################################################
-#       Title: rawimage-datesort.py
+# Title: rawimage-datesort.py
 #   Copyright: Christoph G. Keller <christoph.keller@gmx.net>
 # Description: Sort image files into folder using exif info
 #
@@ -14,7 +14,7 @@ import shutil
 import pyexiv2
 
 
-def processImage(i,outdir):
+def processImage(i, outdir):
     """
     For every image get the capture date,
     create a output folder (yyyy-mm-dd)
@@ -23,62 +23,66 @@ def processImage(i,outdir):
     i - input image
     outdir - where to store the path/images
 
+    :rtype : False on error
     """
 
     # get the meta info
     image = pyexiv2.ImageMetadata(i)
     try:
         image.read()
-    except:
+    except IOError:
+        print ":-( Unable to open file: %s, check your pattern" % (i)
         return False
 
     key = 'Exif.Image.DateTime'
-    datetime=[]
+    datetime = []
     if key in image.exif_keys:
         datetime = image[key]
     else:
+        # This handles android (nexus 5) pictures
         key = 'Exif.Photo.DateTimeOriginal'
-        datetime = image[key]
+        if key in image.exif_keys:
+            datetime = image[key]
+        else:
+            print ":-( There is no exif date/time info, can not sort it."
+            return False
 
     # create the output folder structure
-    datedir = str(datetime.value.year) 
+    datedir = str(datetime.value.year)
     datedir += '-'
-    datedir += str( '%02i') % datetime.value.month
+    datedir += str('%02i') % datetime.value.month
     datedir += '-'
     datedir += str('%02i') % datetime.value.day
-    
-    fulldatedir = os.path.abspath(os.path.join(outdir,datedir))
-    
+
+    fulldatedir = os.path.abspath(os.path.join(outdir, datedir))
+
     # create the outdir
     if not os.path.exists(fulldatedir):
         os.makedirs(fulldatedir)
-    
+
     # move the file
-    if not os.path.exists(os.path.join(fulldatedir,i)):
-        shutil.move(i,fulldatedir)
+    if not os.path.exists(os.path.join(fulldatedir, i)):
+        shutil.move(i, fulldatedir)
     else:
-        print "File already exists"
+        print "File already exists, not doing anything."
+
+    return True
 
 
 def main():
-
     usage = "usage: %prog [options] arg"
     parser = OptionParser(usage)
 
     parser.add_option("-o", "--outdir", dest="outdir",
-                  help="output dir", default = '.')
-    
+                      help="output dir", default='.')
+
     (options, args) = parser.parse_args()
-    
+
     outdir = options.outdir
 
     # now pocess all the command line files
     for i in args:
-        presult = processImage(i,outdir)
-        if presult == False:
-            print "Unable to open file: %s" % (i)
-
-
+        presult = processImage(i, outdir)
 
 
 if __name__ == "__main__":
